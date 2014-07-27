a3dd7701b4217713db7f7183313f9a85358068d2
Diego Borras <jdiego.borras@gmail.com>
2014-07-28 01:12:39 +0200

    R Script

--- run_analysis.R
+++ run_analysis.R
@@ -10,16 +10,16 @@ require("data.table")
 require("reshape2")
 
 ##We load the labels, names and data first.
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels <- read.table("./activity_labels.txt")[,2]
 
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features <- read.table("./features.txt")[,2]
 
 extract_features <- grepl("mean|std", features)
 
 ##Then load and process X_test & y_test data.
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./test/X_test.txt")
+y_test <- read.table("./test/y_test.txt")
+subject_test <- read.table("./test/subject_test.txt")
 
 names(X_test) = features
 
@@ -35,10 +35,10 @@ names(subject_test) = "subject"
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
 ##Load X_train & y_train data.
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+X_train <- read.table("./train/X_train.txt")
+y_train <- read.table("./train/y_train.txt")
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_train <- read.table("./train/subject_train.txt")
 
 names(X_train) = features
 
